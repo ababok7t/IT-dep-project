037068b4b83c12b153d76c0d5141e305d5767ec7
Александр Толдин <aleksandrtoldin@MacBook-Air-Aleksandr.local>
2024-11-17 16:09:55 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module crypto-helper
+
+go 1.23.2
--- main.go
+++ main.go
@@ -20,14 +20,17 @@ type Coin struct {
 
 func getCoinInfo(id string) (Coin, error) {
 	var coin []Coin
+
 	response, responseError := http.Get(fmt.Sprint("https://api.coinlore.net/api/ticker/?id=", id))
 	if responseError != nil {
 		return Coin{}, responseError
 	}
 	defer response.Body.Close()
+
 	byteSlice := make([]byte, 1024)
 	n, _ := response.Body.Read(byteSlice)
 	json.Unmarshal(byteSlice[:n], &coin)
+
 	return coin[0], nil
 }
 
@@ -36,22 +39,28 @@ func getCoinForecast(id string) (string, error) {
 	if gettingError != nil {
 		return "", gettingError
 	}
+
 	priceNow, _ := strconv.ParseFloat(coin.PriceUsd, 64)
 	change1H, _ := strconv.ParseFloat(coin.PercentChange1H, 64)
 	change24H, _ := strconv.ParseFloat(coin.PercentChange24H, 64)
 	change7D, _ := strconv.ParseFloat(coin.PercentChange7D, 64)
+
 	price1H := priceNow / (1 + change1H/100)
 	price24H := priceNow / (1 + change24H/100)
 	price7D := priceNow / (1 + change7D/100)
+
 	logProfit1 := math.Log(priceNow / price1H)
 	logProfit2 := math.Log(price24H / price1H)
 	logProfit3 := math.Log(price7D / price24H)
+
 	midProfit := (logProfit1 + logProfit2 + logProfit3) / 3
+
 	vol1 := math.Pow(logProfit1-midProfit, 2)
 	vol2 := math.Pow(logProfit2-midProfit, 2)
 	vol3 := math.Pow(logProfit3-midProfit, 2)
+
 	volatility := math.Sqrt((vol1 + vol2 + vol3) / 3)
-	//Z := rand.Float64()
+
 	coinForecast := priceNow*math.Exp(midProfit-1/2*math.Pow(volatility, 2)) + volatility*0.0000001
 	return fmt.Sprintf("%.10f", coinForecast), nil
 }
